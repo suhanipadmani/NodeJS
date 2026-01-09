e7591515343432014240e03114b8d143b4a6339f
suhanipadmani <suhanipadmani0903@gmail.com>
2026-01-09 14:30:52 +0530

    commit changes

--- Practice-demos/airbnb/app.js
+++ Practice-demos/airbnb/app.js
@@ -10,6 +10,7 @@ const PORT = 3000;
 app.use(express.urlencoded());
 app.use(userRouter); 
 app.use("/host",hostRouter);
+app.use(express.static(path.join(rootDir,"public")));
 
 app.use((req, res, next) => {
     res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
